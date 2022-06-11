32b2bcc52f2d5ef2cc714c012b87793283789db0
Sczygelski <avamariescz@gmail.com>
2022-06-10 23:42:41 -0500

    window prompts

--- Develop/script.js
+++ Develop/script.js
@@ -19,21 +19,21 @@ generateBtn.addEventListener("click", writePassword);
 
 //things you need
 var passwordlength = function() {
-
+  var asklength = window.prompt('How many characters long would you like your password? Passwords must contain at least 8 and at most 128.')
 }
 
 var passwordupper = function () {
-
+  var askupper = window.prompt('Would you like your password to contain uppercase letters?')
 }
 
 var passwordlower = function () {
-
+  var asklower = window.prompt('Would you like your password to contain lowercase letters?')
 }
 
 var passwordnumeric = function () {
-
+  var asknumeric = window.prompt('Would you like your password to contain numbers?')
 }
 
 var passwordspecial = function () {
-  
+  var askspecial = window.prompt('Would you like your password to contain special characters?')
 }
