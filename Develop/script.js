b10127ed2db9e1019785de6bcf5c1c81e4d4b438
Sczygelski <avamariescz@gmail.com>
2022-06-10 23:32:16 -0500

    sorting out js

--- Develop/script.js
+++ Develop/script.js
@@ -4,14 +4,36 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//things you need
+var passwordlength = function() {
+
+}
+
+var passwordupper = function () {
+
+}
+
+var passwordlower = function () {
+
+}
+
+var passwordnumeric = function () {
+
+}
+
+var passwordspecial = function () {
+  
+}
