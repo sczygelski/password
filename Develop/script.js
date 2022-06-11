e91bc092f2754dca45204b78f6168a1a512647f0
Sczygelski <avamariescz@gmail.com>
2022-06-11 15:01:09 -0500

    added back special characters

--- Develop/script.js
+++ Develop/script.js
@@ -15,6 +15,7 @@
 var passwordupper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var passwordlower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','q','x','y','z'];
 var passwordnumeric = ['1','2','3','4','5','6','7','8','9','0'];
+var passwordspecial = [' ','!','"','#','$','%','&','(',')',')','*','+',',','-','.','/',':',';','<','>','=','?','@','[',']','\','^','`','{','}','|','~'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
